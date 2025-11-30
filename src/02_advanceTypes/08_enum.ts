1ac80eae88bf00dd251a0e17b8a6700a47438291
Sani-Mohibur <mohiburrahmansani@gmail.com>
2025-11-30 16:16:13 +0600

    Rename file & fix comments.

--- src/02_advanceTypes/08_enum.ts
+++ src/02_advanceTypes/08_enum.ts
@@ -16,8 +16,8 @@ console.log(user1);
 
 //Documentation
 //However, running any code that requires transformations, like the code
-//still needs the use of --experimental-transform-types
+// still needs the use of --experimental-transform-types
 
 //Created a js file for this code using "tsc .\file_location"
-//to see the javascript file instead of enum
-//as JavaScript does not have a built-in enum data type
+// to see the javascript file instead of enum
+// as JavaScript does not have a built-in enum data type
